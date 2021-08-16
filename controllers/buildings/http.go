ec1e69f4d00696743556f6b8fb95be2b536b4580
alfi <alfi@twiscoder.com>
2021-08-16 09:57:20 +0700

    add list building

--- controllers/buildings/http.go
+++ controllers/buildings/http.go
@@ -1,14 +1,11 @@
 package buildings
 
 import (
-	"errors"
 	"net/http"
 	"rentRoom/businesses/buildings"
 	controller "rentRoom/controllers"
 	"rentRoom/controllers/buildings/request"
 	"rentRoom/controllers/buildings/response"
-	"strconv"
-	"strings"
 
 	echo "github.com/labstack/echo/v4"
 )
@@ -41,26 +38,18 @@ func (ctrl *BuildingsController) Store(c echo.Context) error {
 	return controller.NewSuccessResponse(c, response.FromDomain(resp))
 }
 
-func (ctrl *BuildingsController) Update(c echo.Context) error {
+func (ctrl *BuildingsController) GetAll(c echo.Context) error {
 	ctx := c.Request().Context()
 
-	id := c.QueryParam("id")
-	if strings.TrimSpace(id) == "" {
-		return controller.NewErrorResponse(c, http.StatusBadRequest, errors.New("missing required id"))
-	}
-
-	req := request.Buildings{}
-	if err := c.Bind(&req); err != nil {
-		return controller.NewErrorResponse(c, http.StatusBadRequest, err)
-	}
-
-	domainReq := req.ToDomain()
-	idInt, _ := strconv.Atoi(id)
-	domainReq.ID = idInt
-	resp, err := ctrl.buildingsUseCase.Update(ctx, domainReq)
+	resp, err := ctrl.buildingsUseCase.GetAll(ctx)
 	if err != nil {
 		return controller.NewErrorResponse(c, http.StatusInternalServerError, err)
 	}
 
-	return controller.NewSuccessResponse(c, response.FromDomain(*resp))
+	responseController := []response.Buildings{}
+	for _, value := range resp {
+		responseController = append(responseController, response.FromDomain(value))
+	}
+
+	return controller.NewSuccessResponse(c, responseController)
 }
